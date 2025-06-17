7c70daa29980b116eb8db2c00741c57f8b18c1dc
orangecyclingstudent <orangevidit@gmail.com>
2025-06-17 18:49:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const videoContainer = document.getElementById('videoContainer');
 const cycleButton = document.getElementById('cycleButton');
 
 const creatorVids = [
-    {name:'RickRoll', vids:['pKskW7wJ0v0']},
     {name: 'Linus Tech Tips', vids:['0zbLw0X5Le4', 'g3Y0lBoOrEg','G2pQV3-JXNU', 'JxrlCBkCLWA', 'cF8lI14sS1I', 'Y3dFCWA16jY', '6wgHq9NZru0', 'PcnWneULGAQ' ]},
+    {name:'RickRoll', vids:['pKskW7wJ0v0']},
     {name:'MKBHD', vids:['qF4vZxbSmjo', 'qF4vZxbSmjo', 'SUeQvl7IOV4']},
     {name:'Mrwhosetheboss', vids:['De7s-IB_DAw', 'fepKQej1DH0', 'cZypFRdPS9M']},
     ];
